eda5f5f65371c4e26a189e2e92f94139a8272f31
bhardier <benoit.hardier@probayes.com>
2021-10-14 10:16:42 +0200

    Add licensing information.

--- openstf/model/regressors/sigmoid.py
+++ openstf/model/regressors/sigmoid.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
 from typing import Union
 import numpy as np
 import numbers
